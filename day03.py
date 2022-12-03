f11f1fbae22f9c70a9f7372f88031a6450f8afa3
Warren Moore <warren@wamonite.com>
2022-12-03 12:16:47 +0000

    day03 part 2

--- day03.py
+++ day03.py
@@ -10,13 +10,17 @@ def conv_char(c):
 
 def parse_func(lines):
     for line in lines:
-        line = [conv_char(c) for c in line]
+        yield [conv_char(c) for c in line]
+
+
+def split_line(lines):
+    for line in lines:
         split = len(line) // 2
         yield line[:split], line[split:]
 
 
 def compartment_intersect(lines):
-    for line in lines:
+    for line in split_line(lines):
         c0 = set(line[0])
         c1 = set(line[1])
         yield c0.intersection(c1)
@@ -26,9 +30,22 @@ def results_1(lines):
     return sum(map(sum, compartment_intersect(lines)))
 
 
+def elf_intersect(lines):
+    while e0 := set(next(lines, [])):
+        e1 = set(next(lines))
+        e2 = set(next(lines))
+        yield e0.intersection(e1).intersection(e2)
+
+
+def results_2(lines):
+    return sum(map(sum, elf_intersect(lines)))
+
+
 def run():
     print_results("data/day03test.txt", results_1, parse_func=parse_func, expected=157)
     print_results("data/day03.txt", results_1, parse_func=parse_func)
+    print_results("data/day03test.txt", results_2, parse_func=parse_func, expected=70)
+    print_results("data/day03.txt", results_2, parse_func=parse_func)
 
 
 if __name__ == "__main__":
