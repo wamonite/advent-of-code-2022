05fd9278ae9a28ad882155d15c052294896a76d9
Warren Moore <warren@wamonite.com>
2022-12-09 07:30:35 +0000

    day08 part 2

--- day08.py
+++ day08.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+from math import prod
+
 import numpy as np
 
 from aocutil import Lines, print_results
@@ -51,6 +53,41 @@ def results_1(trees: np.array) -> Result:
     return len(find_visible_trees(trees))
 
 
+def get_scenic_tree_count(trees: np.array, x: int, y: int) -> int:
+    width = trees.shape[0] - 1
+    height = trees.shape[1] - 1
+
+    tree_height = trees[y, x]
+    ranges = [
+        (range(x - 1, -1, -1), True),
+        (range(x + 1, width + 1), True),
+        (range(y - 1, -1, -1), False),
+        (range(y + 1, height + 1), False),
+    ]
+    scores = []
+    for r, a in ranges:
+        s = 0
+        for pos in r:
+            h = trees[y, pos] if a else trees[pos, x]
+            s += 1
+            if h >= tree_height:
+                break
+        scores.append(s)
+
+    return prod(scores)
+
+
+def results_2(trees: np.array) -> Result:
+    score_max = 0
+    for x in range(0, trees.shape[0]):
+        for y in range(0, trees.shape[1]):
+            score = get_scenic_tree_count(trees, x, y)
+            if score > score_max:
+                score_max = score
+
+    return score_max
+
+
 def run() -> None:
     print_results(
         "data/day08test.txt",
@@ -59,6 +96,13 @@ def run() -> None:
         expected=21,
     )
     print_results("data/day08.txt", results_1, parse_func=parse_func)
+    print_results(
+        "data/day08test.txt",
+        results_2,
+        parse_func=parse_func,
+        expected=8,
+    )
+    print_results("data/day08.txt", results_2, parse_func=parse_func)
 
 
 if __name__ == "__main__":
